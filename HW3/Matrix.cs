c4756fc0097dfe1e0af51a28b50962e62a77ce0d
andrew <a.cherpak-26@kpi.ua>
2022-05-24 20:56:48 +0300

    HW3 task 4 done

--- HW3/Matrix.cs
+++ HW3/Matrix.cs
@@ -1,7 +1,81 @@
+using System;
+
 namespace Vector
 {
     public class Matrix
     {
+        private int[,] _matrix;
+
+        public Matrix(int[,] source)
+        {
+            _matrix = source;
+        }
+
+        public Matrix(int height, int width)
+        {
+            _matrix = new int[height, width];
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i >= 0 && i < _matrix.GetLength(0)) return _matrix[i, j];
+                throw new Exception("Index out of range!");
+            }
+            set
+            {
+                if (i >= 0 && i < _matrix.GetLength(0)) _matrix[i, j] = value;
+                throw new Exception("Index out of range!");
+            }
+        }
         
+        public void DiagonalSnakeFill(Modes mode)
+        {
+            int counter = 1;
+            int[] deltas = { (int)(-1*Math.Pow(-1, (mode==Modes.StartRight?0:1))), (int)Math.Pow(-1, (mode==Modes.StartRight?0:1))};
+            int[] indexes = {0, 0};
+            int[] boundaries = {_matrix.GetLength(0), _matrix.GetLength(1), -1, -1};
+            
+            while (counter <= _matrix.Length)
+            {
+                _matrix[indexes[0], indexes[1]] = counter++;
+                if (!CheckCollisions(indexes, boundaries, deltas, mode))
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        indexes[i] = deltas[i];
+                    }
+                }
+            }
+        }
+        
+        public enum Modes
+        {
+            StartRight = 0,
+            StartDown = 1
+        }
+        
+        private static bool CheckCollision(ref int comparedIndex, int boundary, ref int anotherIndex, ref int cDelta,
+            ref int aDelta)
+        {
+            if (comparedIndex + cDelta != boundary) return false;
+            anotherIndex++;
+            cDelta *= -1;
+            aDelta *= -1;
+            return true;
+        }
+
+        private static bool CheckCollisions(int[] indexes, int[] boundaries, int[] deltas, Modes mode)
+        {
+            for (int ctr = 0; ctr < 4; ctr++)
+            {
+                int i = ctr+(mode==Modes.StartDown?1:0)*(int)Math.Pow(-1, ctr%2);
+                if (CheckCollision(ref indexes[i % 2], boundaries[i], ref indexes[(i + 1) % 2], ref deltas[i % 2],
+                        ref deltas[(i + 1) % 2])) return true;
+            }
+
+            return false;
+        }
     }
 }
