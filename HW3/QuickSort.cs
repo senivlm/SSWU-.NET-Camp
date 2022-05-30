d78f3006c1e2a400b2b9173b22cbf83c92e4736b
andrew <a.cherpak-26@kpi.ua>
2022-05-30 21:53:51 +0300

    Added dividing by first element

--- HW3/QuickSort.cs
+++ HW3/QuickSort.cs
@@ -5,25 +5,41 @@ namespace HW3
         public static void Sort(Vector vector, int startIndex, int excludedEndIndex)
         {
             if (excludedEndIndex-startIndex<2) return;
-            int central = Divide(vector, startIndex, excludedEndIndex);
+            int central = DivideS(vector, startIndex, excludedEndIndex);
             Sort(vector, startIndex, central);
             Sort(vector, central+1, excludedEndIndex);
         }
 
-        private static int Divide(Vector vector, int startIndex, int excludedEndIndex)
+        private static int DivideE(Vector vector, int startIndex, int excludedEndIndex)
         {
             int central = vector[excludedEndIndex-1];
             int firstBiggerIndex = startIndex;
             
-            for (int j = startIndex; j < excludedEndIndex-1; j++) {
-                if (vector[j] <= central) {
-                    (vector[firstBiggerIndex], vector[j]) = (vector[j], vector[firstBiggerIndex]);
+            for (int i = startIndex; i < excludedEndIndex-1; i++) {
+                if (vector[i] <= central) {
+                    (vector[firstBiggerIndex], vector[i]) = (vector[i], vector[firstBiggerIndex]);
                     firstBiggerIndex++;
                 }
             }
 
             (vector[firstBiggerIndex], vector[excludedEndIndex-1]) = (vector[excludedEndIndex-1], vector[firstBiggerIndex]);
             return firstBiggerIndex;
+        }
+        
+        private static int DivideS(Vector vector, int startIndex, int excludedEndIndex)
+        {
+            int central = vector[startIndex];
+            int lastSmallerIndex = excludedEndIndex-1;
+            
+            for (int i = excludedEndIndex-1; i > startIndex; i--) {
+                if (vector[i] >= central) {
+                    (vector[lastSmallerIndex], vector[i]) = (vector[i], vector[lastSmallerIndex]);
+                    lastSmallerIndex--;
+                }
+            }
+
+            (vector[lastSmallerIndex], vector[startIndex]) = (vector[startIndex], vector[lastSmallerIndex]);
+            return lastSmallerIndex;
         }
     }
 }
