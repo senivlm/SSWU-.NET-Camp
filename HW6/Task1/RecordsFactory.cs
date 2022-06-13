fff7d016d1b48a1c41fb34a6f099a7b1e0cd6b7d
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-13 10:49:15 +0300

    Update RecordsFactory.cs

--- HW6/Task1/RecordsFactory.cs
+++ HW6/Task1/RecordsFactory.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 namespace HW6.Task1
 {
     public class RecordsFactory
-    {
+    {// На мій погляд краще мати один контент, який перетворює в один запис. Більш мобільно і працює для великих файлів.
+        Але технічно добре. 
         public static Record[] GetRecords(string[] fileContent)
         {
             int quarter = Int32.Parse(fileContent[0].Split(',')[1]);
@@ -13,6 +14,7 @@ namespace HW6.Task1
             for (i = 0; i < fileContent.Length - 1; i++)
             {
                 records[i] = new Record(quarter, fileContent[i + 1]);
+                // Поясніть! Перервати не хотіли цикл?
                 if (records[i].IsNull) i--;
             } 
             return records[..(i-1)];
